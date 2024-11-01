2565399e4f105134d2bcf9a522c76a069471e9df
Abdul Moiz <moiza0799@gmail.com>
2024-10-31 19:09:35 -0500

    Implemented Stack

--- Algorithims/Program.cs
+++ Algorithims/Program.cs
@@ -1,6 +1,10 @@
 ﻿using Algorithims;
 
 
+
+
+/*
+ *  ------ Binary Search Tree Implementation
 int[] numbersToInsert = { 25, 37, 28, 16, 7, 44, 23, 31, 47, 17, 26, 24, 11, 1, 38 };
 var binaryTree = new BinaryTree(numbersToInsert[0]);
 for (int i = 1; i < numbersToInsert.Length; i++)
@@ -10,3 +14,19 @@ for (int i = 1; i < numbersToInsert.Length; i++)
 binaryTree.PreOrder();
 binaryTree.PostOrder();
 binaryTree.InOrder();
+*/
+
+
+// Stack Implmenetation
+int[] numbersToInsert = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+var stack = new Stack();
+foreach (int number in numbersToInsert)
+{
+    stack.Push(number);
+}
+while (!stack.IsEmpty())
+{
+    Console.WriteLine(stack.Pop());
+}
+
+

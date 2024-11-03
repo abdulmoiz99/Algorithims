a47708f458ab7b068732cbc472b1bfaa7af126d5
Abdul Moiz <moiza0799@gmail.com>
2024-11-03 15:10:20 -0600

    Find the middle element of double linked list

---
+++ Algorithims/DoublyLinkedList.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace Algorithims
+{
+    internal class DoublyLinkedList
+    {
+        Node header;
+
+        public void Insert(int value)
+        {
+            Node current = header;
+            if (current == null)
+            {
+                var Node = new Node();
+                Node.value = value;
+                header = Node;
+            }
+            else
+            {
+                while (current.next != null)
+                {
+                    current = current.next;
+                }
+                var Node = new Node();
+                Node.value = value;
+                Node.previous = current;
+                current.next = Node;
+            }
+        }
+        public void Print()
+        {
+            Node current = header;
+
+            while(current != null)
+            {
+                Console.Write(current.value + ",");
+                current = current.next;
+            }
+            Console.WriteLine();
+        }
+
+        public int FindMiddleElement()
+        {
+            Node slow = header;
+            Node fast = header;
+            
+            while(fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next?.next;
+            }
+
+            return slow.value;
+        }
+
+
+        class Node
+        {
+            public Node previous;
+            public int value;
+            public Node next;
+        }
+    }
+
+}
--- Algorithims/Program.cs
+++ Algorithims/Program.cs
@@ -117,6 +117,22 @@ binaryTree.InOrder();
 
 
 
+/// 
+// Implemtation Of Doubly Linked List
+
+//var list = new DoublyLinkedList();
+
+//list.Insert(1);
+//list.Insert(2);
+//list.Insert(3);
+//list.Insert(4);
+//list.Insert(5);
+
+//list.Print();
+
+//Console.WriteLine(list.FindMiddleElement());
+
+
 
 
 

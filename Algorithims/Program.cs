bf3460c0662f52eadb9a3fcfd2b1955cf8119388
Abdul Moiz <moiza0799@gmail.com>
2024-11-15 07:37:12 -0600

    Added initial structure for graph

--- Algorithims/Program.cs
+++ Algorithims/Program.cs
@@ -518,20 +518,38 @@ binaryTree.InOrder();
 
 
 
-var fibonacciStore = new Dictionary<int, int>();
+//var fibonacciStore = new Dictionary<int, int>();
 
-Console.WriteLine(Fibonacci(40));
+//Console.WriteLine(Fibonacci(40));
 
-int Fibonacci(int n)
-{
-    if (n == 0) return 0;
-    else if (n == 1) return 1;
+//int Fibonacci(int n)
+//{
+//    if (n == 0) return 0;
+//    else if (n == 1) return 1;
 
-    if (!fibonacciStore.ContainsKey(n))
-    {
-        int result = Fibonacci(n - 1) + Fibonacci(n - 2);
-        fibonacciStore[n] = result;
-    }
+//    if (!fibonacciStore.ContainsKey(n))
+//    {
+//        int result = Fibonacci(n - 1) + Fibonacci(n - 2);
+//        fibonacciStore[n] = result;
+//    }
 
-    return fibonacciStore[n];
-}
+//    return fibonacciStore[n];
+//}
+
+
+var graph = new Graph();
+
+graph.AddVertex(1);
+graph.AddVertex(2);
+graph.AddVertex(3);
+graph.AddVertex(4);
+graph.AddVertex(5);
+
+graph.AddEdge(1, 2);
+graph.AddEdge(1, 4);
+graph.AddEdge(1, 5);
+graph.AddEdge(2, 4);
+graph.AddEdge(3, 5);
+graph.AddEdge(4, 5);
+
+graph.PrintGraph();
